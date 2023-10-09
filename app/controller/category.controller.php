0d40e90473f864ce7fbb2364b58f7577a45c094c
Felipe Barreyra <felipebarreyra@gmail.com>
2023-10-09 16:27:36 -0300

    agregue category controler y category model

---
+++ app/controller/category.controller.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'app/model/category.model.php';
+require_once 'app/view/category.view.php';
+
+class CategoryController{
+    private $model;
+    private $view;
+
+    public function __construct(){
+        //me aseguro que este logueado
+        //AuthHelper::verify();
+        session_start();
+        $this->model = new CategoryModel();
+        $this->view = new CategoryView();
+    }
+    public function showAllCategories(){
+       
+        $categories = $this->model->getAllCategories();
+        if(!empty($categories)){
+
+            $this->view->showAllCategories($categories);
+
+        }else{
+
+            echo "Error: No hay categorias";
+        }
+
+    }
+}
---
+++ app/model/category.model.php
@@ -0,0 +1,48 @@
+<?php 
+
+class CategoryModel {
+
+    private $db;
+
+    public function __construct(){
+
+        require_once 'config_db/db.php';
+        $conn = new db();
+        $this->db = $conn->connection();
+
+    }
+
+    public function getAllCategories(){
+
+        $query = $this->db->prepare('SELECT * FROM category');
+
+        $query->execute();
+
+        $category = $query->fetchAll(PDO::FETCH_OBJ);
+
+        return $category;
+    }
+    public function getCategoryBySeasson($season = 'verano') {
+
+        $query = $this->db->prepare('SELECT * FROM category WHERE season = ?');
+    
+        $query->execute([$season]);
+    
+        $category = $query->fetch(PDO::FETCH_OBJ);
+    
+        return $category;
+    }
+    public function getCategoryBySeasson($season = 'invierno') {
+
+        $query = $this->db->prepare('SELECT * FROM category WHERE season = ?');
+    
+        $query->execute([$season]);
+    
+        $category = $query->fetch(PDO::FETCH_OBJ);
+    
+        return $category;
+    }
+    public function deleteCategory($)
+    
+
+
